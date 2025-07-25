2d0964e1f779b6ad338e74c52c29cb8a27fe21c5
yasui tatsuya <yasui.nagaoka@gmail.com>
2025-07-26 05:50:47 +0900

    fix : ビルドエラーの解消

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Homepage from './components/common/Homepage';
+import Login from './components/common/Login';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Login />
     <Homepage />
   </React.StrictMode>
 );
